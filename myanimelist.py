d033111f8541837f5c9b4d72128ed0e7f39270c7
Luis Checa <edhaker13@gmail.com>
2013-02-03 18:36:23 +0000

    Experimental json version probably breaks

--- myanimelist.py
+++ myanimelist.py
@@ -8,10 +8,11 @@
 # http://github.com/Flexget/Flexget/blob/master/flexget/plugins/input/trakt_list.py
 # and the BeatifulSoup Documentation (it's for bs3)
 # http://www.crummy.com/software/BeautifulSoup/bs3/documentation.html
-
+from __future__ import unicode_literals, division, absolute_import
 import logging
-from flexget.utils.soup import get_soup
-from flexget.utils.tools import urlopener
+import re
+from requests import RequestException
+from flexget.utils import json
 from flexget.utils.cached_input import cached
 from flexget.plugin import register_plugin, PluginError
 from flexget.entry import Entry
@@ -36,44 +37,60 @@ class MyAnimeList(object):
 
     Option username are required. Anime list must be public.
     """
+    anime_map = {
+        'title': 'title',
+        'watched_status': 'status'
+                 }
 
     def validator(self):
         from flexget import validator
         root = validator.factory('dict')
         root.accept('text', key='username', requried=True)
+        root.accept('choice', key='list').accept_choices(['watching', 'plan to watch'])
         return root
 
     @cached('myanimelist', persist='2 hour')
     def on_task_input(self, task, config):
-        url_params = config.copy()
-        if 'username' in config:
-            url_params['username'] = config['username']
-            url_params['section'] = 'watching'
-            url_params['sort'] = 'by name'
-        else:
+        if not 'username' in config:
             raise PluginError('Must define the list username to retrieve from MAL')
-
-        url = 'http://myanimelist.net/animelist/'
-        if url_params['username']:
-            url+= config['username']
-        if url_params['section'] == 'watching':
-            url+='&status=1'
-        if url_params['sort'] == 'by name':
-            url+='&order=1'
-
+    
+        username = config['username']
+        map = self.anime_map
+        if not 'list' in config:    
+            status = 'watching'
+        else:
+            status = config['list']
+        
+        url = 'http://mal-api.com/animelist/%s' % username
+        if 'password' in config:
+            auth = {'username': config['username'],
+                    'password': config['password']}
         entries = []
         log.verbose("Retrieving MyAnimeList on %s ."  % url)
         
-        page = urlopener(url, log)
-        soup = get_soup(page)
+        try:
+            task.requests.get(url)
+        except RequestException as e:
+            raise PluginError('Could not retrieve list from MAL (%s)' % e.message)
+        data = json.dumps(url)
+        if not data:
+            #check_auth()
+            log.warning('No data returned from trakt.')
+            return
+        
+        if not isinstance(data['items'], list):
+            raise PluginError('Faulty items in response: %s' % data['items'])
+        data = data['items']
+        for item in data:
+            if item['watched_status'] == status:
+                    item = item['anime']
+            entry = Entry()
+            entry.update_using_map(map, item)
+            if entry.isvalid():
+                # Remove non alphanumeric and space characters
+                entry['title'] = re.sub('[^a-zA-Z0-9 ]', '', entry['title'])
+                entries.append(entry)
 
-        if soup.div('', "badresult"):
-            raise PluginError('Anime list isn\'t public.')
-        for a in soup.find_all('a', 'animetitle'):
-            title = a.span.string.replace('.', '').replace('-', ' ')
-            entry= Entry()
-            entry['title'] = title
-            entries.append(entry)
-            return entries
+        return entries
 
 register_plugin(MyAnimeList, 'myanimelist', api_ver=2)
