9ae37909c856548986f19ccf75a0d6710314cef8
Luis Checa <edhaker13@gmail.com>
2013-09-12 14:15:05 +0100

    Refactor to use @internet, added logging
    @internet decorator removes the need for related exception catching
    Use a dict to map source values to desired entry fields
    Encode the query using urlencode
    Add simple syntax, value is query
    Index counter is not necessary
    Make MyAnimeList anime url with the captured id
    Image url ending in t is a small image

--- search_myanimelist.py
+++ search_myanimelist.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals, division, absolute_import
 import logging
 from urllib import urlencode
-from requests import RequestException
 from flexget.utils.cached_input import cached
 from flexget.plugin import register_plugin, PluginError, internet
 from flexget.entry import Entry
@@ -9,7 +8,7 @@ from flexget.entry import Entry
 log = logging.getLogger('search_myanimelist')
 
 
-class MyAnimeListSearch(object):
+class SearchMyAnimeList(object):
     """A simple search MyAnimeList.net input plugin for FlexGet.
 
     Creates an entry for each item found by the queries.
@@ -22,7 +21,7 @@ class MyAnimeListSearch(object):
 
       search_myanimelist:
         - <query1>
-        - <query2>.
+        - <query2>
 
     Example::
 
@@ -30,6 +29,13 @@ class MyAnimeListSearch(object):
         from:
           search_myanimelist: Free!
     """
+    anime_map = {
+        'mal_id': 'id',
+        'title': 'title',
+        'description': 'synopsis',
+        'mal_type': 'type',
+        'mal_episodes': 'episodes'
+    }
 
     def validator(self):
         from flexget import validator
@@ -52,31 +58,24 @@ class MyAnimeListSearch(object):
         queries = self.get_config(config)
         entries = []
         for query in queries:
-            query = urlencode(query, 0)
-            url = 'http://mal-api.com/anime/search?q=%s' % query
+            query = urlencode({'q': query, '': ''})
+            url = 'http://mal-api.com/anime/search?%s' % query
             log.verbose("Searching MyAnimeList for %s ." % query)
-            try:
-                data = task.requests.get(url).json()
-            except RequestException as e:
-                raise PluginError('Could not retrieve query from MAL (%s)' % e.message)
+            data = task.requests.get(url).json()
             if not data:
-                log.warning('No data returned from MAL.')
+                log.warning('No data returned from MyAnimeList.')
                 return None
-
-            if not isinstance(data['anime'], list):
-                raise PluginError('Faulty items in response: %s' % data['anime'])
-                # reduces list nesting
-            data = data['anime']
-            # JSON indexing starts at 0
-            i = 0
+            if not isinstance(data, list):
+                raise PluginError('Faulty items in response: %s' % data)
             for item in data:
-                if isinstance(item['id'], int):
-                    title = data[i]['title']
-                    entry = Entry()
-                    entry['title'] = title
-                    entries.append(entry)
-                    i += 1
+                entry = Entry()
+                entry.update_using_map(self.anime_map, item)
+                entry['mal_image_url'] = item['image_url'].replace('t.jpg', '.jpg')
+                mal_url = 'http://myanimelist.net/anime/%s' % item['id']
+                entry['url'] = mal_url
+                entry['mal_url'] = mal_url
+                entries.append(entry)
         return entries
 
 
-register_plugin(MyAnimeListSearch, 'search_myanimelist', api_ver=2)
+register_plugin(SearchMyAnimeList, 'search_myanimelist', api_ver=2)
